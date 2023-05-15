cfcbedf4383df7ec42ec9e1fd2f9e60b1fd72889
Maksim Irkha <mirha93@gmail.com>
2023-05-15 10:23:05 +0300

    added a loop with a post condition to the GallowsInputOutput.readLetter method

--- src/GallowsInputOutput.java
+++ src/GallowsInputOutput.java
@@ -15,23 +15,21 @@ public class GallowsInputOutput {
         reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
     }
     public char readLetter() {
-        System.out.println("Введите букву: ");
-        String input = "";
-        try {
 
-            input = reader.readLine().toUpperCase();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        while (input.trim().length() != 1 || !Character.isLetter(input.trim().charAt(0))) {
-            System.out.println("Неверный ввод. Введите букву: ");
+        String input;
+        do {
+            System.out.println("Введите букву: ");
+            input = "";
             try {
                 input = reader.readLine().toUpperCase();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            if (input.trim().length() != 1 || !Character.isLetter(input.trim().charAt(0))) {
+                System.out.println("Неверный ввод. Введите букву: ");
+            }
         }
+        while (input.trim().length() != 1 || !Character.isLetter(input.trim().charAt(0)));
 
         return input.charAt(0);
     }
